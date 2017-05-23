6fb050d7c56f8870a54027374606fa876bbdfae3
BazzaLuca <lucabazza123@gmail.com>
2017-05-23 21:40:10 +0200

    SignUp - coach registration

--- app/src/main/java/com/example/cesare/leagueoflegendscoaching/CoachRegistration.java
+++ app/src/main/java/com/example/cesare/leagueoflegendscoaching/CoachRegistration.java
@@ -12,11 +12,23 @@ public class CoachRegistration extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coach_registration);
 
-        // Create spinner
+        // Create elo spinner
         Spinner eloSpinner = (Spinner) findViewById(R.id.elo_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.elo_list, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         eloSpinner.setAdapter(adapter);
+
+        //create roles spinner
+        Spinner role1Spinner = (Spinner) findViewById(R.id.role1_spinner);
+        ArrayAdapter<CharSequence> r1Adapter = ArrayAdapter.createFromResource(this, R.array.role1_list, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        role1Spinner.setAdapter(r1Adapter);
+
+        Spinner role2Spinner = (Spinner) findViewById(R.id.role2_spinner);
+        ArrayAdapter<CharSequence> r2Adapter = ArrayAdapter.createFromResource(this, R.array.role2_list, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        role2Spinner.setAdapter(r2Adapter);
+
     }
 
 }
