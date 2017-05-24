f99f2406baa6bcf176553b1022deaed200234eb4
cece95 <cesaregri@gmail.com>
2017-05-24 10:21:24 +0200

    server connection and signup

---
+++ app/src/main/java/com/example/cesare/leagueoflegendscoaching/ServerConnection.java
@@ -0,0 +1,102 @@
+package com.example.cesare.leagueoflegendscoaching;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.security.NoSuchAlgorithmException;
+
+
+/**
+ * Created by cesare on 24/05/2017.
+ */
+
+public class ServerConnection {
+    //Domain url
+    final static String domain = "https://league-of-legends-coaching.herokuapp.com/";
+
+    public static int userRegistration(String name, String ign, String password){
+        int control = 0;
+        String sha1_password = null;
+
+        //request url creation
+        String route = "userRegistration/";
+        String complete_url = domain + route;
+        URL url = null;
+
+        try {
+            url = new URL(complete_url);
+        } catch (MalformedURLException e) {
+            control = 3;
+            e.printStackTrace();
+        }
+
+        try {
+            sha1_password = Security.SHA1(password);
+        } catch (NoSuchAlgorithmException e) {
+            control = 1;
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            control = 2;
+            e.printStackTrace();
+        }
+
+        if (url != null){
+            try {
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setDoOutput(true);
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setUseCaches(false);
+                urlConnection.setConnectTimeout(10000);
+                urlConnection.setReadTimeout(10000);
+                urlConnection.setRequestProperty("Content-Type","application/json");
+
+                urlConnection.connect();
+
+                JSONObject jsonParam = new JSONObject();
+                jsonParam.put("name", name);
+                jsonParam.put("ign", ign);
+                jsonParam.put("password", sha1_password);
+                OutputStreamWriter out = new   OutputStreamWriter(urlConnection.getOutputStream());
+                out.write(jsonParam.toString());
+                out.close();
+
+                int HttpResult = urlConnection.getResponseCode();
+                if(HttpResult ==HttpURLConnection.HTTP_OK){
+                    StringBuilder sb = new StringBuilder();
+                
+                    BufferedReader br = new BufferedReader(new InputStreamReader(
+                            urlConnection.getInputStream(),"utf-8"));
+                    String line = null;
+                    while ((line = br.readLine()) != null) {
+                        sb.append(line + "\n");
+                    }
+                    br.close();
+
+                    if (sb.toString().equals("ok")){
+                        control = 200;
+                    }
+            }
+        } catch (UnsupportedEncodingException e) {
+                control = 4;
+                e.printStackTrace();
+            } catch (IOException e) {
+                control = 5;
+                e.printStackTrace();
+            } catch (JSONException e) {
+                control = 6;
+                e.printStackTrace();
+            }
+        }
+
+        return control;
+}
--- app/src/main/java/com/example/cesare/leagueoflegendscoaching/SignUp.java
+++ app/src/main/java/com/example/cesare/leagueoflegendscoaching/SignUp.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class SignUp extends Activity {
 
@@ -31,12 +32,34 @@ public class SignUp extends Activity {
     }
 
     Intent coachIntent(Context context, CheckBox coach) {
-        Intent risIntent;
+        Intent risIntent = null;
         if (coach.isChecked()) {
             risIntent = new Intent(context, CoachRegistration.class);
         }
         else {
-            risIntent = new Intent(context, StudentArea.class);
+            EditText name_input = findViewById(R.id.name_input);
+            EditText inGame_input = findViewById(R.id.inGame_input);
+            EditText password_input = findViewById(R.id.password_input);
+            EditText repeatPassword_input = findViewById(R.id.repeatPassword_input);
+
+            String name = name_input.getText().toString();
+            String ign = inGame_input.getText().toString();
+            String password = password_input.getText().toString();
+            String r_password = repeatPassword_input.getText().toString();
+
+            if (password.equals(r_password)){
+                int registration = ServerConnection.userRegistration(name, ign, password);
+                if (registration == 200){
+                    risIntent = new Intent(context, StudentArea.class);
+                    risIntent.putExtra("user", name);
+                }
+                else {
+                    //errore registrazione fallita
+                }
+            }
+            else{
+                //errore password non coincidono
+            }
         }
         return risIntent;
     }
