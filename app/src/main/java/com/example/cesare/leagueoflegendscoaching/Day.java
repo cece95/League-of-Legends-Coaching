b8191da43e52ef1c9c2ae9f5e6246f19b1b9fc3a
cece95 <cesaregri@gmail.com>
2017-06-06 21:13:44 +0200

    Aggiunti Day Orario e Periodo per la gestione degli orari del coach Aggiunta libreria Joda Time per la gestione (più avanti) degli intervalli

---
+++ app/src/main/java/com/example/cesare/leagueoflegendscoaching/Day.java
@@ -0,0 +1,10 @@
+package com.example.cesare.leagueoflegendscoaching;
+
+/**
+ * Created by cesare on 06/06/2017.
+ */
+
+public enum Day {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY,
+    THURSDAY, FRIDAY, SATURDAY
+}
---
+++ app/src/main/java/com/example/cesare/leagueoflegendscoaching/Orario.java
@@ -0,0 +1,15 @@
+package com.example.cesare.leagueoflegendscoaching;
+
+/**
+ * Created by cesare on 06/06/2017.
+ */
+
+public class Orario {
+    Day day;
+    Periodo periodo;
+
+    public Orario(Day day, int inizio, int fine){
+        this.day = day;
+        this.periodo = new Periodo(inizio, fine);
+    }
+}
