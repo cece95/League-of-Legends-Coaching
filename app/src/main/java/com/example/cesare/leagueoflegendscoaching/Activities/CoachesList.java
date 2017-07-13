bd3e94757dd64a85b1bb4ec998c555940b8fa37b
BazzaLuca <lucabazza123@gmail.com>
2017-07-13 11:21:20 +0200

    FIND COACH - ScrollView

---
+++ app/src/main/java/com/example/cesare/leagueoflegendscoaching/Activities/CoachesList.java
@@ -0,0 +1,17 @@
+package com.example.cesare.leagueoflegendscoaching.Activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.cesare.leagueoflegendscoaching.R;
+
+public class CoachesList extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coaches_list);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/cesare/leagueoflegendscoaching/Classes/CoachFrame.java
@@ -0,0 +1,36 @@
+package com.example.cesare.leagueoflegendscoaching.Classes;
+
+import android.media.Image;
+
+import com.example.cesare.leagueoflegendscoaching.Types.Elo;
+import com.example.cesare.leagueoflegendscoaching.Types.Language;
+import com.example.cesare.leagueoflegendscoaching.Types.Role;
+
+import java.util.HashSet;
+
+/**
+ * Created by asus on 12/07/2017.
+ */
+
+public class CoachFrame {
+    String name;
+    Elo elo;
+    Role role1;
+    Role role2;
+    HashSet<Language> languages;
+    Boolean favorite;
+
+    Image eloImage;
+
+
+    public CoachFrame(String name, Elo elo, Role role1, Role role2, HashSet<Language> languages, Boolean favorite) {
+        this.name = name;
+        this.elo = elo;
+        this.role1 = role1;
+        this.role2 = role2;
+        this.languages = languages;
+        this.favorite = favorite;
+    }
+
+
+}
