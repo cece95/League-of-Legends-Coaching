5575acfd35750eef51c44996cd90602d9a49b5ae
cece95 <cesaregri@gmail.com>
2017-08-01 13:01:09 +0200

    CoachesList

--- app/src/main/java/com/example/cesare/leagueoflegendscoaching/Classes/CoachFrame.java
+++ app/src/main/java/com/example/cesare/leagueoflegendscoaching/Classes/CoachFrame.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.TextView;
 
 import com.example.cesare.leagueoflegendscoaching.R;
@@ -44,20 +45,43 @@ public class CoachFrame {
         //frame
         LinearLayout frame = new LinearLayout(context);
         frame.setOrientation(LinearLayout.HORIZONTAL);
+        frame.setWeightSum(3);
+        LayoutParams params = (LayoutParams) frame.getLayoutParams();
+        params.width = LayoutParams.MATCH_PARENT;
+        params.height = LayoutParams.WRAP_CONTENT;
+        frame.setLayoutParams(params);
 
-        //image
-        ImageView eloSymbol = new ImageView(context);
-        eloSymbol.setImageBitmap(eloImage);
-        frame.addView(eloSymbol);
+        // Elo Image Layout
+        LinearLayout imageLayout = new LinearLayout(context);
+        LayoutParams imageParams = (LayoutParams) imageLayout.getLayoutParams();
+        imageParams.weight = 1;
+        imageParams.height = LayoutParams.WRAP_CONTENT;
+        imageParams.width = 0;
+        // Elo Image
+        ImageView eloSymbol = getEloImage();
+
+        imageLayout.setLayoutParams(imageParams);
+        imageLayout.addView(eloSymbol);
 
         //info
         LinearLayout infoBox = new LinearLayout(context);
+        infoBox.setWeightSum(2);
         infoBox.setOrientation(LinearLayout.VERTICAL);
+        LayoutParams infoBoxParams = (LayoutParams) infoBox.getLayoutParams();
+        infoBoxParams.weight = 2;
+        infoBoxParams.width = 0;
+        infoBoxParams.height = LayoutParams.WRAP_CONTENT;
+        infoBox.setLayoutParams(infoBoxParams);
         frame.addView(infoBox);
 
         //name + cost
         LinearLayout nameCost = new LinearLayout(context);
         nameCost.setOrientation(LinearLayout.HORIZONTAL);
+        LayoutParams nameCostParams = (LayoutParams) nameCost.getLayoutParams();
+        nameCostParams.weight = 1;
+        nameCostParams.width = LayoutParams.WRAP_CONTENT;
+        nameCostParams.height = 0;
+        nameCost.setLayoutParams(nameCostParams);
 
         TextView name = new TextView(context);
         name.setText(this.name);
@@ -73,6 +97,11 @@ public class CoachFrame {
         //role + detalils
         LinearLayout roleDetails = new LinearLayout(context);
         roleDetails.setOrientation(LinearLayout.HORIZONTAL);
+        LayoutParams roleDetailsParams =  (LayoutParams) roleDetails.getLayoutParams();
+        roleDetailsParams.weight = 1;
+        roleDetailsParams.width = LayoutParams.WRAP_CONTENT;
+        roleDetailsParams.height = 0;
+        roleDetails.setLayoutParams(roleDetailsParams);
 
         TextView role = new TextView(context);
         role.setText(role1+"/"+role2);
@@ -86,6 +115,27 @@ public class CoachFrame {
         return frame;
     }
 
+    private ImageView getEloImage() {
+        ImageView img = null;
+        switch (this.elo) {
+            case 0 : img.setBackgroundResource(R.mipmap.badge_bronze);
+                break;
+            case 1 : img.setBackgroundResource(R.mipmap.badge_silver);
+                break;
+            case 2 : img.setBackgroundResource(R.mipmap.badge_gold);
+                break;
+            case 3 : img.setBackgroundResource(R.mipmap.badge_platinum);
+                break;
+            case 4 : img.setBackgroundResource(R.mipmap.badge_diamond);
+                break;
+            case 5 : img.setBackgroundResource(R.mipmap.badge_master);
+                break;
+            case 6 : img.setBackgroundResource(R.mipmap.badge_challenger);
+                break;
+        }
+        return img;
+    }
+
     private HashSet<Language> getLanguages(JSONObject json) throws JSONException {
         HashSet<Language> res = new HashSet<>();
 
@@ -95,5 +145,4 @@ public class CoachFrame {
 
         return res;
     }
-
 }
